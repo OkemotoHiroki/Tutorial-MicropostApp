2596550f73166603fc860201998adf1a032831b8
OkemotoHiroki <okemoto@antas.jp>
2026-05-14 12:48:53 +0900

    I18n対応のためのアプリ名とタイトルのローカライズを実施

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,11 +7,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_response :success
+    assert_select "meta[name=?][content=?]", "application-name", I18n.t("app.name")
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    expected_title = "#{I18n.t("static_pages.contact.title")} | #{I18n.t("app.base_title")}"
+    assert_select "title", expected_title
   end
 end
